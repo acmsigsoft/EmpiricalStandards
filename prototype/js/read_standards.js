4550e75f1f0349f234c467506dc833bfb2ceeb2b
Taher Ahmed Ghaleb <taher.a.ghaleb@gmail.com>
2021-01-15 15:34:45 -0500

    Some improvements to the JavaScript code

--- prototype/js/read_standards.js
+++ prototype/js/read_standards.js
@@ -28,25 +28,32 @@ function readEmpiricalStandards(){
 	return empirical_standards;
 }
 
-function convert_checklists_to_checkboxes(checklistText){
+function convert_checklists_to_checkboxes(standardName, checklistName, checklistText){
 	var checkboxes = document.createElement("UL");
-	
+	checkboxes.style = "list-style-type:none;";
+
 	lines = checklistText.split("<br/>");
+	var i = 0;
 	for(let line of lines){
-		if (line.trim() != ""){
-			var checkbox = document.createElement("input");
+		line_text = line.trim().replace("- ", "").replace("-\t", " ").replace(/\^[0-9]+\^/, "");
+		if (line_text != ""){
+			i++;
+			checkbox_id = standardName + "-" + checklistName + ":" + i;
+			var checkboxLI = document.createElement("LI");
+			var checkboxInput = document.createElement("input");
 			var checkboxLabel = document.createElement("label");
-			checkbox.setAttribute("type", "checkbox");
-			checkbox.setAttribute("name", "check");
-			checkbox.setAttribute("style", "color:#FFF");
-			checkbox.setAttribute("value", line.trim());
-			checkboxLabel.setAttribute("for", "check");
-			checkboxLabel.innerHTML = line.trim() + "<br/>";
-			checkboxes.appendChild(checkbox);
-			checkboxes.appendChild(checkboxLabel);
+			checkboxInput.type = "checkbox";
+			checkboxInput.id = checkbox_id;
+			checkboxInput.name = checkbox_id;
+			checkboxInput.style = "color:#FFF";
+			checkboxInput.value = line_text;
+			checkboxLabel.htmlFor = checkbox_id;
+			checkboxLabel.appendChild(document.createTextNode(line_text));
+			checkboxLI.appendChild(checkboxInput);
+			checkboxLI.appendChild(checkboxLabel);
+			checkboxes.appendChild(checkboxLI);
 		}
 	}
-	
 	return checkboxes;
 }
 function generateStandardChecklist(){
@@ -55,8 +62,8 @@ function generateStandardChecklist(){
 	dom.innerHTML = empirical_standards;
 	var standardTags = dom.getElementsByTagName("standard");
 	var form = document.createElement("FORM");
-	form.setAttribute("id", "checklists");
-	form.setAttribute("name", "standards");
+	form.id = "checklists";
+	form.name = "checklists";
 	for (let standardTag of standardTags){
 		let standardName = "\"" + standardTag.getAttribute('name') + "\"";
 		if (keys.includes(standardName)){
@@ -76,12 +83,16 @@ function generateStandardChecklist(){
 													   replaceAll(">", "").
 													   replaceAll("\n\n", "<br/>").replaceAll("\n- ", "<LI>");
 				
-				checkboxes = convert_checklists_to_checkboxes(checklistText)
+				checkboxes = convert_checklists_to_checkboxes(standardTag.getAttribute('name'), checklistTag.getAttribute('name'), checklistText)
 				checklistTitleUL.appendChild(checkboxes);
 				form.appendChild(checklistTitleUL);
 			}
-			document.body.appendChild(form);
-			//document.getElementById("fromFile").innerHTML = standardTag.innerHTML;
 		}
 	}
+	var submit = document.createElement("input");
+	submit.type = "submit";
+	submit.id = "checklist_submit";
+	submit.name = "checklist_submit";
+	form.appendChild(submit);
+	document.body.appendChild(form);
 }
