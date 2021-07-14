fa5d4e1c41e38d96a645a2eee584187a469d66b2
Taher Ahmed Ghaleb <taher.a.ghaleb@gmail.com>
2021-07-14 14:18:31 -0400

    Mark all as yes (for testers). Fixes #40

--- form_generator/js/read_standards.js
+++ form_generator/js/read_standards.js
@@ -6,7 +6,7 @@ function getParameterByName(param_name, url = window.location.href){
 	for (var param_index in params){
 		var param = params[param_index].split("=");
 		if(param[0] === param_name){
-			param_values[i] = "\"" + unescape(param[1]) + "\"";
+			param_values[i] = param.length > 1 ? "\"" + unescape(param[1]) + "\"" : "noval";
 			i++;
 		}
 	}
@@ -628,6 +628,7 @@ function generate_two_phase_reviewer_deviation_block(checklistItem_id) {
 }
 
 function convert_standard_checklists_to_html_checklists(standardName, checklistName, checklistText, footnotes){
+	tester = getParameterByName('y')[0] == 'noval' ? true : false;
 	var checklists = document.createElement("UL");
 	var standard_H3 = document.createElement("B");
 	standard_H3.style = "font-size:20px;";
@@ -679,6 +680,7 @@ function convert_standard_checklists_to_html_checklists(standardName, checklistN
 				checklistRadioNo.type = "radio";
 				checklistRadioYes.value = "yes";
 				checklistRadioNo.value = "no";
+				checklistRadioYes.checked = tester;
 
 				// Generate a deviation block
 				var deviation_block;
@@ -937,7 +939,8 @@ function generateStandardChecklist(){
 		document.body.appendChild(container);
 	else
 		wrapper.appendChild(container);
-		
+	
+	show_hide_decision_message();
 }
 //download the file as a checklist
 function saveFile(){
